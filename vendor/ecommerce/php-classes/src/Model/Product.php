52c2f32845cdc4822d84084c7c1914e289acd8d9
thiagovalentedasilva <69157334+thiagovalentedasilva@users.noreply.github.com>
2020-12-18 12:33:56 -0300

    erro ao editar e salvar produto

--- vendor/ecommerce/php-classes/src/Model/Product.php
+++ vendor/ecommerce/php-classes/src/Model/Product.php
@@ -6,12 +6,12 @@ use \thiagova\Mailer;
 
 class Product extends Model {
 
-    public static function listAll(){
+   public static function listAll(){
         $sql = new Sql();
         return $sql->select("SELECT * from tb_products ORDER BY desproduct");
     }
 
-    public function save (){
+     public function save (){
         $sql = new Sql();
         $results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", array(
             ":idproduct"=>$this->getidproduct(),
@@ -43,13 +43,13 @@ class Product extends Model {
 
     public function checkPhoto(){
         if(file_exists(
-        $_SERVER['DOCUMENT_ROOT']. DIRECTORY_SEPARATOR.
-        "res" .DIRECTORY_SEPARATOR. 
+        $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR.
+        "res" .DIRECTORY_SEPARATOR . 
         "site" . DIRECTORY_SEPARATOR . 
         "img" . DIRECTORY_SEPARATOR . 
-        "products" . DIRECTORY_SEPARATOR.
-        $this->getidproduct().".jpg")){
-            $url =  "/res/site/img/products/".$this->getidproduct().".jpg";
+        "products" . DIRECTORY_SEPARATOR .
+        $this->getidproduct() .".jpg")){
+            $url =  "/res/site/img/products/".$this->getidproduct() . ".jpg";
         } else {
             $url =  "/res/site/img/product.jpg";
         }
@@ -71,15 +71,15 @@ class Product extends Model {
         switch ($extension){
             case "jpg":
             case "jpeg":
-                $image = imagecreatefromjpeg($file[tmp_name]);
+                $image = imagecreatefromjpeg($file["tmp_name"]);
             break;
 
             case "gif":
-                $image = imagecreatefromgif($file[tmp_name]);
+                $image = imagecreatefromgif($file["tmp_name"]);
             break;
             
             case "png":
-                $image = imagecreatefrompng($file[tmp_name]);
+                $image = imagecreatefrompng($file["tmp_name"]);
             break;
         }
         $dist =  $_SERVER['DOCUMENT_ROOT']. DIRECTORY_SEPARATOR.
